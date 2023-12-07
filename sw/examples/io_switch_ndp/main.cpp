a618a517fa5fb1891fb2a0a1b0662d497da6b5ed
Harshavardhan Unnibhavi <harshanavkis@gmail.com>
2023-12-07 13:07:18 +0000

    IO Switch: add comments for matrix multiplication use case
    Signed-off-by: Harshavardhan Unnibhavi <harshanavkis@gmail.com>

--- sw/examples/io_switch_ndp/main.cpp
+++ sw/examples/io_switch_ndp/main.cpp
@@ -13,8 +13,8 @@
 
 using namespace fpga;
 
-constexpr auto const ddefSize = 4096;
-constexpr auto const odefSize = 4096;
+constexpr auto const ddefSize = 64 * 64 * 8;
+constexpr auto const odefSize = 64 * 64 * 8 * 2;
 
 int main(int argc, char *argv[])
 {
@@ -31,28 +31,28 @@ int main(int argc, char *argv[])
 
     uint64_t fpga_data = 345679821;
 
-    dMem = (uint64_t *)cproc.getMem({CoyoteAlloc::HOST_2M, (uint32_t)n_input_pages});
-    oMem = (uint64_t *)cproc.getMem({CoyoteAlloc::HUGE_2M, (uint32_t)n_output_pages});
+    // dMem = (uint64_t *)cproc.getMem({CoyoteAlloc::HOST_2M, (uint32_t)n_input_pages});
+    // oMem = (uint64_t *)cproc.getMem({CoyoteAlloc::HUGE_2M, (uint32_t)n_output_pages});
     fMem = (uint64_t *)cproc.getMem({CoyoteAlloc::HUGE_2M, (uint32_t)n_output_pages});
 
-    memcpy(dMem, &fpga_data, 8);
+    // memcpy(dMem, &fpga_data, 8);
     // memcpy(oMem, &fpga_data, 8);
     memcpy(fMem, &fpga_data, 8);
 
-    std::cout << "Host memory data mapped at: " << dMem << std::endl;
-    std::cout << "Host memory data mapped at: " << oMem << std::endl;
+    // std::cout << "Host memory data mapped at: " << dMem << std::endl;
+    // std::cout << "Host memory data mapped at: " << oMem << std::endl;
     std::cout << "FPGA memory data mapped at: " << fMem << std::endl;
 
-    std::cout << "dMem before:" << *((uint64_t *)dMem) << std::endl;
-    std::cout << "oMem before:" << *((uint64_t *)oMem) << std::endl;
+    // std::cout << "dMem before:" << *((uint64_t *)dMem) << std::endl;
+    // std::cout << "oMem before:" << *((uint64_t *)oMem) << std::endl;
     std::cout << "fMem before:" << *((uint64_t *)fMem) << std::endl;
 
     /* Using host memory */
     // cproc.ioSwDbg();
     // cproc.ioSwitch(IODevs::HOST_MEM);
     // cproc.ioSwDbg();
-    // cproc.invoke({CoyoteOper::TRANSFER, (void *)dMem, o_data_size});
-    // std::cout << "In host mem: dMem after:" << *((uint64_t *)dMem) << std::endl;
+    // cproc.invoke({CoyoteOper::TRANSFER, (void *)fMem, (void *)fMem, 64 * 64 * 8 * 2, 64 * 64 * 8});
+    // // std::cout << "In host mem: dMem after:" << *((uint64_t *)dMem) << std::endl;
     // std::cout << "In host mem: fMem after:" << *((uint64_t *)fMem) << std::endl;
 
     /*****************************************************************************/
@@ -65,15 +65,17 @@ int main(int argc, char *argv[])
     /* First config: offload and sync to same memory */
     /* Works and only fMem is updated immediately*/
     std::cout << "Offload and sync to same region" << std::endl;
-    cproc.invoke({CoyoteOper::OFFLOAD, (void *)fMem, o_data_size, true, true, 0, false});
+    cproc.invoke({CoyoteOper::OFFLOAD, (void *)fMem, 64 * 64 * 8 * 2, true, true, 0, false});
     std::cout << "In FPGA mem: fMem after OFFLOAD:" << *((uint64_t *)fMem) << std::endl;
-    cproc.invoke({CoyoteOper::READ, (void *)fMem, o_data_size, true, true, 0, false});
+    cproc.invoke({CoyoteOper::READ, (void *)fMem, 64 * 64 * 8 * 2, true, true, 0, false});
     std::cout << "In FPGA mem: fMem after READ:" << *((uint64_t *)fMem) << std::endl;
-    cproc.invoke({CoyoteOper::WRITE, (void *)fMem, o_data_size, true, true, 0, false});
+    // For matmul change to 64*64*8
+    cproc.invoke({CoyoteOper::WRITE, (void *)fMem, 64 * 64 * 8 * 2, true, true, 0, false});
     std::cout << "In FPGA mem: fMem after WRITE:" << *((uint64_t *)fMem) << std::endl;
-    cproc.invoke({CoyoteOper::SYNC, (void *)fMem, o_data_size, true, true, 0, false});
+    // For matmul change to 64*64*8
+    cproc.invoke({CoyoteOper::SYNC, (void *)fMem, 64 * 64 * 8 * 2, true, true, 0, false});
     std::cout << "In FPGA mem: fMem after SYNC:" << *((uint64_t *)fMem) << std::endl;
-    std::cout << "In FPGA mem: oMem after:" << *((uint64_t *)oMem) << std::endl;
+    // std::cout << "In FPGA mem: oMem after:" << *((uint64_t *)oMem) << std::endl;
     std::cout << "In FPGA mem: fMem after:" << *((uint64_t *)fMem) << std::endl;
 
     /* Second config: offload and sync to different regions */
