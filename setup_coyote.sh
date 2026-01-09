163ceebfaee2ebde5cb7251cde7482a65d438aee
Harshavardhan Unnibhavi <harshanavkis@gmail.com>
2026-01-09 13:31:55 +0000

    Add scripts for deploying and running coyote

---
+++ setup_coyote.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# sudo bash sw/util/hot_reset.sh "e1:00.0"
+
+echo 1 | sudo tee /sys/bus/pci/devices/0000:e1:00.0/remove
+echo 1 | sudo tee /sys/bus/pci/rescan
+
+sudo insmod driver/build/coyote_driver.ko
